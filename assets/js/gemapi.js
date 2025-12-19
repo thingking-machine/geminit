7ef12bf9802b7ae7b3b853ab170b841a04559ca6
alxfed <alxfed@gmail.com>
2025-12-18 18:24:54 -0600

    initial commit

--- assets/js/gemapi.js
+++ assets/js/gemapi.js
@@ -15,8 +15,7 @@ self.onmessage = async function (event) {
 		{'category': 'HARM_CATEGORY_HATE_SPEECH', 'threshold': 'BLOCK_NONE'},
 		{'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT', 'threshold': 'BLOCK_NONE'},
 		{'category': 'HARM_CATEGORY_DANGEROUS_CONTENT', 'threshold': 'BLOCK_NONE'},
-		{'category': 'HARM_CATEGORY_HARASSMENT', 'threshold': 'BLOCK_NONE'},
-		{'category': 'HARM_CATEGORY_CIVIC_INTEGRITY', 'threshold': 'BLOCK_NONE'}
+		{'category': 'HARM_CATEGORY_HARASSMENT', 'threshold': 'BLOCK_NONE'}
 	]
 
 	try {
